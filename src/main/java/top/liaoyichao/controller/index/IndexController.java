0f0ed6d2e9104766fa0dbcd018fc02e8c11a6017
g <1406937973@qq.com>
2019-03-27 15:57:21 +0800

    原版项目，在此基础上进行修改

---
+++ src/main/java/top/liaoyichao/controller/index/IndexController.java
@@ -0,0 +1,26 @@
+package top.liaoyichao.controller.index;
+import com.jfinal.core.Controller;
+
+import top.liaoyichao.util.MySession;
+
+/**
+ * IndexController 指向系统访问首页
+ * @author jbolt.cn and Liao
+ * @date 2019年3月1日
+ */
+public class IndexController extends Controller {
+	/**
+	 * 首页Action
+	 * 
+	 */
+	public void index() {
+		
+		if(getSessionAttr(MySession.getSessionKey()) != null){
+			//代表标识session 跳转页面 并提示 请先登陆
+			setAttr("msg", "请先登陆~");
+			renderFreeMarker("index.html");
+		}else{
+			render("index.html");
+		}
+	}
+}
